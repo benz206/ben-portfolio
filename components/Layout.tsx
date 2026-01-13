caaf6a54838cd173ea5d7fc937046c5667ae1bcc
Ben Zhou <ben.zhou.2006@gmail.com>
2026-01-13 12:17:28 -0500

    Remove old use client directive

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,8 @@
-"use client";
-
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
-import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: PropsWithChildren) {
-    const pathname = usePathname();
-    if (pathname === "/") {
-        return (
-            <div className="relative h-screen bg-[#050506]">
-                <Navigation />
-                <main className="h-full overflow-y-auto home-scroll snap-y snap-proximity">
-                    {children}
-                    <Footer />
-                </main>
-            </div>
-        );
-    }
     return (
         <div className="scroll-smooth">
             <Navigation />
