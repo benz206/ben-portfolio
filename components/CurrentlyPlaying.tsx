baff5a90dab9a6d496e2bfdbd660a949391378ab
Ben Zhou <ben.zhou.2006@gmail.com>
2026-05-05 00:07:00 -0400

    Fixes

--- components/CurrentlyPlaying.tsx
+++ components/CurrentlyPlaying.tsx
@@ -107,7 +107,7 @@ const isPlaying = track.paused !== "true";
                     <div className="absolute inset-0 bg-black/50" />
                 </>
             )}
-<div className="relative z-10 flex items-center space-x-4 flex-1 min-w-0">
+            <div className="relative z-10 flex items-center pl-4 space-x-4 flex-1 min-w-0">
                 <div className="overflow-hidden w-20 h-20 rounded-xl shadow-xl shrink-0">
                     {track.albumArt && (
                         <Image
